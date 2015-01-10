45b5e7b1a3d64eb50f5588e9cbf6745ae91526bb
erith <erith9@gmail.com>
2015-01-10 22:56:15 +0900

    - 1st registration source code.

---
+++ CsApp/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace CsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ct01 = new System.Windows.Forms.ComboBox();
+            this.ct02 = new System.Windows.Forms.ComboBox();
+            this.tb01 = new System.Windows.Forms.TextBox();
+            this.btn_click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ct01
+            // 
+            this.ct01.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ct01.FormattingEnabled = true;
+            this.ct01.Location = new System.Drawing.Point(22, 24);
+            this.ct01.Name = "ct01";
+            this.ct01.Size = new System.Drawing.Size(193, 20);
+            this.ct01.TabIndex = 0;
+            this.ct01.SelectedIndexChanged += new System.EventHandler(this.ct01_SelectedIndexChanged);
+            // 
+            // ct02
+            // 
+            this.ct02.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ct02.FormattingEnabled = true;
+            this.ct02.Location = new System.Drawing.Point(240, 24);
+            this.ct02.Name = "ct02";
+            this.ct02.Size = new System.Drawing.Size(166, 20);
+            this.ct02.TabIndex = 1;
+            // 
+            // tb01
+            // 
+            this.tb01.Location = new System.Drawing.Point(22, 62);
+            this.tb01.Multiline = true;
+            this.tb01.Name = "tb01";
+            this.tb01.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.tb01.Size = new System.Drawing.Size(263, 338);
+            this.tb01.TabIndex = 2;
+            // 
+            // btn_click
+            // 
+            this.btn_click.Location = new System.Drawing.Point(460, 433);
+            this.btn_click.Name = "btn_click";
+            this.btn_click.Size = new System.Drawing.Size(75, 23);
+            this.btn_click.TabIndex = 3;
+            this.btn_click.Text = "입력";
+            this.btn_click.UseVisualStyleBackColor = true;
+            this.btn_click.Click += new System.EventHandler(this.btn_click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 468);
+            this.Controls.Add(this.btn_click);
+            this.Controls.Add(this.tb01);
+            this.Controls.Add(this.ct02);
+            this.Controls.Add(this.ct01);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ct01;
+        private System.Windows.Forms.ComboBox ct02;
+        private System.Windows.Forms.TextBox tb01;
+        private System.Windows.Forms.Button btn_click;
+
+    }
+}
+
---
+++ CsApp/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Xml;
+using NPOI.XSSF.Model; // InternalWorkbook
+using NPOI.XSSF.UserModel; // HSSFWorkbook, HSSFSheet
+
+namespace CsApp
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument menu = new XmlDocument();
+        XSSFWorkbook wb;
+ 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 폼로드시
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //메뉴 XML 파일 로드
+            var path = Application.StartupPath + @"\menu.xml";            
+            menu.Load(path);
+            var nodes = menu.SelectNodes("/menulist/menu");
+            foreach(XmlNode node in nodes)
+            {
+                ct01.Items.Add(node.Attributes["title"].Value);
+            }
+
+        }
+
+
+        /// <summary>
+        /// 콤보박스 1 선택시 이벤트
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ct01_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var idx = ct01.SelectedIndex;
+            var nodes = menu.SelectNodes("/menulist/menu[" + idx + "]/menulist/menu");
+            ct02.Items.Clear();
+            foreach (XmlNode node in nodes)
+            {
+                ct02.Items.Add(node.Attributes["title"].Value);
+            }
+        }
+
+        //저장 버튼 클릭
+        private void btn_click_Click(object sender, EventArgs e)
+        {
+            //파일 경로 읽어오기
+            var savepath = Application.StartupPath + @"\DATA.xlsx";
+
+
+            //워크북에 엑셀파일 로드하기
+            using (var file = new FileStream(savepath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                wb = new XSSFWorkbook(file);
+            }
+            var sheet = wb.GetSheetAt(0);
+
+            //마지막에 입력된 로우번호를 가지고 온다.
+            int row_count = sheet.PhysicalNumberOfRows;
+
+            //윈도우 폼에 입력된 글자 가져오기
+            XSSFRichTextString str = new XSSFRichTextString(tb01.Text);
+
+            //시트에 로우 만들기.
+            var row = sheet.CreateRow(row_count);
+            var cell = row.CreateCell(0);
+            cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+            var cs =wb.CreateCellStyle();
+            cs.WrapText = true;
+            cell.CellStyle = cs;
+            cell.SetCellValue(str);
+            
+            //워크시트 파일로 저장하기
+            using (var file2 = new FileStream(savepath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                wb.Write(file2);
+                file2.Close();
+            }	
+        }
+    }
+}
